ddaf44aade6024b388fa3ebcb377d1a5ba6c3031
gagtain <r.tis@list.ru>
2023-10-03 01:53:20 +0300

    refactor

--- api/project-wallet/wallet/api/serializers/monero.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class MoneroGetBallanceSerializer(serializers.Serializer):
-
-    account_index = serializers.IntegerField()
--- api/project-wallet/wallet/api/services/base.py
+++
@@ -1,8 +0,0 @@
-from global_modules.exeptions import CodeDataException
-
-
-def get_field_in_dict_or_exception(data: dict, field, message, code=400):
-    if data.get(field):
-        return data[field]
-    else:
-        raise CodeDataException(error=message, status=code)
