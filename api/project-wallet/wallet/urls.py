fddefa94bd0f391f021afb7c113732d7b1ec5bf6
gagtain <r.tis@list.ru>
2023-10-03 01:56:16 +0300

    refactor

--- api/project-wallet/wallet/urls.py
+++ api/project-wallet/wallet/urls.py
@@ -5,6 +5,5 @@ from django.urls import path
 from wallet.api.views import MoneroAPI
 
 urlpatterns = [
-    path('monero/ballance', MoneroAPI.as_view({'get': 'get_balance'})),
-    path('monero/transfer', MoneroAPI.as_view({'post': 'create_transaction'}))
+    path('monero/ballance', MoneroAPI.as_view({'get': 'get_balance'}))
 ]
