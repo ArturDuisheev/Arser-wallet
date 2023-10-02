4095a3956b63e0e13eda92426e5488db70e566ef
gagtain <r.tis@list.ru>
2023-10-02 23:20:50 +0300

    Payment

---
+++ api/project-wallet/wallet/choices.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class NetWorkChoice(models.TextChoices):
+    USDT = "USDT"
+    BTC = "BTC"
+    MONERO = "MONERO"
+    TON = "TON"
+
+
+class CurrencyChoice(models.TextChoices):
+    USD = "USD"
+    EUR = "EUR"
+    RUB = "RUB"
+
